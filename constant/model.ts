0989621842f167437f26cf40fcf4819b8d1a0f10
peanut996 <peanut996@peanut996.com>
2025-03-26 00:59:01 +0800

    Update model.ts

--- constant/model.ts
+++ constant/model.ts
@@ -1,6 +1,7 @@
 export const Model: Record<string, string> = {
   'gemini-2.0-flash': 'Gemini 2.0 Flash',
   'gemini-2.0-flash-lite': 'Gemini 2.0 Flash Lite',
+  'gemini-2.5-pro-exp-03-25': 'Gemini 2.5 Pro',
   'gemini-1.5-pro': 'Gemini 1.5 Pro',
   'gemini-1.5-pro-latest': 'Gemini 1.5 Pro Latest',
   'gemini-1.5-flash': 'Gemini 1.5 Flash',
